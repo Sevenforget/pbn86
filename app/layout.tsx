1b7f105dcaf98d9d92403457084a698fcb93fd4d
AhnHyesook <samaishit@naver.com>
2025-09-06 14:58:59 +0900

    feat: API 기반 데이터 로딩으로 변환 완료

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,26 @@
-import type { Metadata } from 'next'
-import './globals.css'
+import type { Metadata } from "next";
+import "./globals.css";
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
-}
+  title: "v0 App",
+  description: "Created with v0",
+  generator: "v0.dev",
+
+  verification: {
+    other: {
+      "naver-site-verification": "212481a62910f57da9291006cf7553c7048aff27",
+    },
+  },
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  )
+  );
 }
